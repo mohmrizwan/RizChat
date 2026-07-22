226d5355643fb84d68d19cc50648fbe87e5a2efa
mohmrizwan <151992791+mohmrizwan@users.noreply.github.com>
2026-07-22 12:34:32 +0530

    edit

--- backend/routes/conversationRoutes.js
+++ backend/routes/conversationRoutes.js
@@ -1,17 +1,22 @@
 import express from "express";
 import isLoggedIn from "../middleware/isLoggedIn.js";
-import { createConversation } from "../controllers/conversationController.js";
+import {
+  createConversation,
+  getUserConversations,
+} from "../controllers/conversationController.js";
 import {
   sendPrivateMessage,
   getPrivateMessages,
   deletePrivateMessage,
   seenPrivateMessages,
+  editPrivateMessage,
 } from "../controllers/PrivateMessage.js";
 import upload from "../middleware/multer.js";
 
 const router = express.Router();
 
 router.post("/start", isLoggedIn, createConversation);
+router.get("/allConversations", isLoggedIn, getUserConversations);
 router.post(
   "/sendMessage",
 
@@ -20,6 +25,7 @@ router.post(
   sendPrivateMessage,
 );
 router.get("/getMessages/:conversationId", isLoggedIn, getPrivateMessages);
+router.put("/editPrivateMessage/:messageId", isLoggedIn, editPrivateMessage);
 router.delete(
   "/deletePrivateMessage/:messageId",
   isLoggedIn,
